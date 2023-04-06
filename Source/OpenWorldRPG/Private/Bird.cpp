d2c69e522e599bdd80b4697f01da5d56b8fe3f20
최영후 <dudgn6392@naver.com>
2023-04-06 13:41:04 +0900

    Crow Skeletal & Anim

---
+++ Source/OpenWorldRPG/Private/Bird.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+ABird::ABird()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	capsule->SetCapsuleHalfHeight(20.f);
+	capsule->SetCapsuleRadius(15.f);
+	SetRootComponent(capsule);
+
+	birdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	birdMesh->SetupAttachment(capsule);
+
+}
+
+// Called when the game starts or when spawned
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/OpenWorldRPG/Public/Bird.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+
+UCLASS()
+class OPENWORLDRPG_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABird();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* capsule;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* birdMesh;
+};

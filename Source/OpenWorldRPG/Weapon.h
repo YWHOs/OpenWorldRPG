bd1881b73ee0294ccbbfb08e1332fb06b30af93d
최영후 <dudgn6392@naver.com>
2023-04-16 02:22:14 +0900

    Weapon Class (Item)Override

---
+++ Source/OpenWorldRPG/Weapon.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDRPG_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+	
+};

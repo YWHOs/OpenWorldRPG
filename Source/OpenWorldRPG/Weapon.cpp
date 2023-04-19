0e69326b3116c7404872bc465ba15df0e1af90ec
최영후 <dudgn6392@naver.com>
2023-04-20 00:49:09 +0900

    Box Trace

--- Source/OpenWorldRPG/Weapon.cpp
+++ Source/OpenWorldRPG/Weapon.cpp
@@ -6,11 +6,27 @@
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
 #include "Components/BoxComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 AWeapon::AWeapon()
 {
 	weaponBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Weapon Box"));
 	weaponBox->SetupAttachment(GetRootComponent());
+	weaponBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	weaponBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	weaponBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	boxTraceStart = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace Start"));
+	boxTraceStart->SetupAttachment(GetRootComponent());
+	boxTraceEnd = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace End"));
+	boxTraceEnd->SetupAttachment(GetRootComponent());
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	weaponBox->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnBoxOverlap);
 }
 void AWeapon::Equip(USceneComponent* _parent, FName _socketName)
 {
@@ -39,4 +55,15 @@ void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
+
+void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FVector start = boxTraceStart->GetComponentLocation();
+	const FVector end = boxTraceEnd->GetComponentLocation();
+
+	TArray<AActor*> actorsToIgnore;
+	actorsToIgnore.Add(this);
+	FHitResult hit;
+	UKismetSystemLibrary::BoxTraceSingle(this, start, end, FVector(5.f, 5.f, 5.f), boxTraceStart->GetComponentRotation(), ETraceTypeQuery::TraceTypeQuery1, false, actorsToIgnore, EDrawDebugTrace::ForDuration, hit, true);
 }
--- Source/OpenWorldRPG/Weapon.h
+++ Source/OpenWorldRPG/Weapon.h
@@ -21,9 +21,13 @@ public:
 	void Equip(USceneComponent* _parent, FName _socketName);
 	void AttachMeshToSocket(USceneComponent* _parent, const FName& _socketName);
 protected:
+	virtual void BeginPlay() override;
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
@@ -31,4 +35,9 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
 	UBoxComponent* weaponBox;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* boxTraceStart;
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* boxTraceEnd;
 };

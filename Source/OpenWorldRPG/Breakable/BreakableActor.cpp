cf63511c1ebf35d5d982c8d65f977275180b7d23
최영후 <dudgn6392@naver.com>
2023-04-22 22:27:12 +0900

    Breakable Actor

---
+++ Source/OpenWorldRPG/Breakable/BreakableActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BreakableActor.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
+
+// Sets default values
+ABreakableActor::ABreakableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	geometryCollection = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeometryCollection"));
+	SetRootComponent(geometryCollection);
+
+	geometryCollection->SetGenerateOverlapEvents(true);
+	geometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+}
+
+// Called when the game starts or when spawned
+void ABreakableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABreakableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABreakableActor::GetHit(const FVector& _point)
+{
+
+}
+

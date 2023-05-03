5aeaf902cf78d9015fae44cd9b4f1282d3139237
최영후 <dudgn6392@naver.com>
2023-05-03 22:06:32 +0900

    Item Soul Class

---
+++ Source/OpenWorldRPG/Soul.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Soul.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDRPG_API ASoul : public AItem
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
